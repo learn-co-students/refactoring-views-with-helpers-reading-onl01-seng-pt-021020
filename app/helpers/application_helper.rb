b0898e82b346039a22c324c9a77b266689579b65
Amber Kerawala Torres <amber_kerawala@berkeley.edu>
2020-07-06 17:52:37 +0000

    Done.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  
+  def title(text)
+    content_for :title, text
+  end
+  
+  #We use the Rails content_for helper within our custom helper here. Helpers on helpers on helpers! What this will do is send our text to the place in our application layout that is expecting some content for the :title.
+  
 end
